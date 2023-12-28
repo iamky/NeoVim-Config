aa816c400f0262f8f6214db13b531133673b94a6
iamky <yadav.kanishk01@gmail.com>
2023-12-28 10:03:39 +0530

    Added Linters & Formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript"},
+        ensure_installed = {"lua", "javascript","c","cpp","css","html","http","ini","json","php","scss","sql","typescript"},
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },  
       })
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,10 @@
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
+vim.cmd("set autoindent")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.cmd("set relativenumber")
+vim.cmd("set number")
+vim.opt.guifont = { "Monaco", ":16" }
